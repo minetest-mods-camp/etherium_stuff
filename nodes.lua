b3ee6ba5413893b3e4c36ffd0fafabfc6fd849b8
Aleri Kaisattera <alerikaisattera@protonmail.com>
2022-09-27 19:18:49 +0600

    Remove sand group from Etherium Sand

--- nodes.lua
+++ nodes.lua
@@ -26,7 +26,7 @@ end
 minetest.register_node("etherium_stuff:sand", {
 	description = S("Etherium Sand"),
 	tiles = {"etherium_sand.png"},
-	groups = {crumbly = 3, falling_node = 1, sand = 1},
+	groups = {crumbly = 3, falling_node = 1},
 	sounds = default.node_sound_sand_defaults(),
 	on_secondary_use = place_in_air
 })
